dbc4d12f5bcdb3e7e414d1e27736310dde4c4fb1
rahbal <rahulrdb18@gmail.com>
2022-06-07 10:20:04 +0530

    fdsaf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 // import components here
 import { SignUpComponent } from './sign-up/sign-up.component'
 import { LoginComponent } from './login/login.component'
+import { HomeComponent } from './home/home.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-// separate Material Module made by Rahul Bali
+// separate Material Module added by Rahul Bali
 import { MaterialModule } from './material.module';
-import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [AppComponent, SignUpComponent, LoginComponent, PageNotFoundComponent, HomeComponent],
